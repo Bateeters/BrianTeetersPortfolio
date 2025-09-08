cd95dc654fb107167645b1a0c623a34dca50806a
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-09-08 18:29:03 -0400

    Updated Stats section with content

--- portfolio/src/components/Stats.jsx
+++ portfolio/src/components/Stats.jsx
@@ -7,32 +7,32 @@ function Stats() {
                 <div className="col-3 p-0 stat-card-container d-flex align-items-end">
                     <div className="card stat-card-start w-100">
                         <div className="stat-card-body text-center">
-                            <h3>stat 1</h3>
-                            <p>extra stuff</p>
+                            <h3>Development</h3>
+                            <p>2+ Years of Professional Experience</p>
                         </div>
                     </div>
                 </div>
                 <div className="col-3 p-0 stat-card-container d-flex align-items-end">
                     <div className="card stat-card w-100">
                         <div className="stat-card-body text-center">
-                            <h3>stat 1</h3>
-                            <p>extra stuff</p>
+                            <h3>Cross-Disciplinary</h3>
+                            <p>Development, Design, & Marketing</p>
                         </div>
                     </div>
                 </div>
                 <div className="col-3 p-0 stat-card-container d-flex align-items-end">
                     <div className="card stat-card w-100">
                         <div className="stat-card-body text-center">
-                            <h3>stat 1</h3>
-                            <p>extra stuff</p>
+                            <h3>Design</h3>
+                            <p>7+ Years of Professional Experience</p>
                         </div>
                     </div>
                 </div>
                 <div className="col-3 p-0 stat-card-container d-flex align-items-end">
                     <div className="card stat-card-end w-100">
                         <div className="stat-card-body text-center">
-                            <h3>stat 1</h3>
-                            <p>extra stuff</p>
+                            <h3>Project Lifecycle</h3>
+                            <p>From Wireframes to Deployment</p>
                         </div>
                     </div>
                 </div>
